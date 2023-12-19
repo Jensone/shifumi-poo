9d3d92b992e41f63930b26ba4c479c6b69d6440b
Jensone <hello@jensone.com>
2023-12-19 18:17:28 +0100

    🐛 corriger(index.php) : ajouter une variable pour compter le nombre total d'histoires 🐛 corriger(_stories.php) : échapper les caractères spéciaux dans les valeurs des histoires ✨ fonctionnalité(_stories.php) : afficher la durée écoulée depuis la date de l'histoire ✨ fonctionnalité(game.php) : afficher le résultat du jeu avec une image correspondante ✨ fonctionnalité(home.php) : afficher le numéro de la partie en cours ou "00" s'il n'y a pas encore de partie

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ use Models\Game;
 
 $stories = (new Story())->getAll('Story', 'DESC');
 $choices = (new Game())->getChoices();
+$count = (new Story())->countAll('Story');
 
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
     $result = (new Game())->play($_POST['choice']);
@@ -18,6 +19,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
         ->setDate((new DateTime('now'))->format('d/m/Y H:i:s'));
         $story->save();
     }
+    
     include __DIR__ . '/src/templates/game.php';
 
 } else {
--- src/templates/home.php
+++ src/templates/home.php
@@ -1,10 +1,10 @@
 <?php include_once __DIR__ . '/components/header.php'; ?>
 <h1>Shifumi</h1>
-<h5>Partie n°<?= 'X' ?></h5>
+<h5>Partie n°<?= $count ? $count : '00' ?></h5>
 <form action="" method="post">
     <?php
     foreach ($choices as $choice) {
-        echo '<input type="submit" name="choice" value="' . $choice . '" class="choice">';
+        echo '<input type="submit" name="choice" value="' . $choice . '" class="choice" data-choice="' . $choice . '">';
     }
     ?>
 </form>
