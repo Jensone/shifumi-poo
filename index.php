f29d246a1052a0631ddaefc10896a7e3a3abb164
Jensone <hello@jensone.com>
2023-12-19 17:11:04 +0100

    🐛 corriger(index.php) : modifier l'ordre de tri des histoires récupérées de 'DESC' à 'ASC' ✨ fonctionnalité(index.php) : ajouter la logique de jeu lorsque la méthode de requête est POST

--- index.php
+++ index.php
@@ -5,7 +5,21 @@ require_once __DIR__ . '/src/autoloading.php';
 use Models\Story;
 use Models\Game;
 
-$stories = (new Story())->getAll('Story');
+$stories = (new Story())->getAll('Story', 'DESC');
 $choices = (new Game())->getChoices();
 
-include __DIR__ . '/src/templates/home.php';
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $result = (new Game())->play($_POST['choice']);
+    if($result) {
+        $story = new Story();
+        $story->setResult($result['result'])
+        ->setComputerChoice($result['computer'])
+        ->setUserChoice($_POST['choice'])
+        ->setDate((new DateTime('now'))->format('d/m/Y H:i:s'));
+        $story->save();
+    }
+    include __DIR__ . '/src/templates/game.php';
+
+} else {
+    include __DIR__ . '/src/templates/home.php';
+}
